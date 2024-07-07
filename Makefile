02aedc72049ac5e8d35979e110132c4eb60c460b
scatter27-lab <chyvashok98@gmail.com>
2024-07-07 13:06:47 +0300

    add new script brain-even and fix code with lint

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ install:
 brain-games:
 	poetry run brain-games
 
+brain-even:
+	poetry run brain-even
+
 build:
 	poetry build
 
@@ -13,4 +16,5 @@ publish:
 package-install:
 	python3 -m pipx install dist/*.whl
 
-
+lint:
+	poetry run flake8 brain_games
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 def welcome_user():
-	print("Welcome to the Brain Games!")
-	name = ''
-	while name == '':
-		print('May I have your name? ', end = '')
-		name = input()
-	print('Hello, ', name)
+    print("Welcome to the Brain Games!")
+    name = ''
+    while name == '':
+        print('May I have your name? ', end='')
+        name = input()
+    print('Hello, ', name)
+
 
 def main():
-	welcome_user()
+    welcome_user()
+
 
 if __name__ == '__main__':
-	main()
+    main()
