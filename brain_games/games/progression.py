9d8f8169a2fef04628cb33565662d4c61ec7fc02
scatter27-lab <chyvashok98@gmail.com>
2024-07-23 20:39:14 +0300

    fix progression

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -3,17 +3,17 @@ from brain_games.engine import run_game
 from brain_games.utils import randint
 
 
-def question_and_result():
+def question_and_answer():
     progression = [randint(0, 100)]
-    hidden_position = randint(0, PROGRESSION_LENGTH)
+    hidden_position = randint(0, PROGRESSION_LENGTH - 1)
     step_of_progression = randint(1, 25)
-    progression = list(range(progression[0], progression[0]
-                             + (step_of_progression * PROGRESSION_LENGTH),
+    progression = list(range(progression[0], progression[0] +
+                             (step_of_progression * PROGRESSION_LENGTH),
                              step_of_progression))
-    result = progression[hidden_position]
+    answer = progression[hidden_position]
     progression[hidden_position] = '..'
-    return ' '.join(map(str, progression)), str(result)
+    return ' '.join(map(str, progression)), str(answer)
 
 
 def run_progression_game():
-    run_game(question_and_result, RULES_PROGRESSION)
+    run_game(question_and_answer, RULES_PROGRESSION)
