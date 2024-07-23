0c34014e7f7a528cad91409c8550adc338e27821
scatter27-lab <chyvashok98@gmail.com>
2024-07-23 12:25:09 +0300

    fix engine

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -6,14 +6,14 @@ def run_game(get_question_and_answer, RULES):
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}! \n{RULES}')
     for _ in range(ROUNDS_NUMBER):
-        question, result = get_question_and_answer()
+        question, answer = get_question_and_answer()
         print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
-        if user_answer == result:
+        if user_answer == answer:
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{result}'.\n"
+                  f"Correct answer was '{answer}'.\n"
                   f"Let's try again, {name}!")
             return
     print(f'Congratulations, {name}!')
